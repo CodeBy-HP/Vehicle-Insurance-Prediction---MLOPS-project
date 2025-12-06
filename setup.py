38de658a54fedd3c9a997baed2408a28543b6369
Harsh Patel <code.by.hp@gmail.com>
2025-12-06 18:44:43 +0530

    project structure ready , venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Harsh Patel",
+    author_email="code.by.hp@gmail.com",
+    packages=find_packages()
+)
